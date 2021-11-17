c43c59cb809d6e770188e2aaf9f7ace090aa8c24
simonaboyadzhie <91485562+simonaboyadzhieva@users.noreply.github.com>
2021-11-17 16:05:22 +0000

    New

--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -5,5 +5,32 @@ import java.util.Scanner;
 public class PasswordCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        
+        boolean passwordValid = false;
+        do{
+          System.out.println("Enter password: ");
+          String password = input.nextLine();
+          if(password.length() < 8){
+            continue;
+          }
+          boolean allLettersAndDigits = true;
+          int digitsCount = 0;
+          for (int index = 0; index < password.length(); index++){
+            char symbol = password.charAt(index);
+            boolean letterOrDigit = Character.isLetterOrDigit(symbol);
+            if (!letterOrDigit){
+              allLettersAndDigits = false;
+              break;
+            }
+            boolean digit = Character.isDigit(symbol);
+            if (digit) {
+              digitsCount++;
+            }
+          }
+          if (allLettersAndDigits && digitsCount >= 2){
+            passwordValid = true;
+          }
+        } while (!passwordValid);
     }
 }
